6adae92789bd1c041ebc5236b52b09c570a90ecd
mordir <ozime.yeah.net>
2019-07-10 19:38:56 +0800

    lodash

--- miao/lodash/mordir-lodash.js
+++ miao/lodash/mordir-lodash.js
@@ -1,5 +1,8 @@
 var mordir = {
+	chunk: function(ary, size = 1) {
+		ary.chunk(array, [size=1])
+	},
 	compact: function(ary) {
-		return ary.filter(it => it)
+		ary.compact([0, 1, false, 2, '', 3]);
 	}
 }
